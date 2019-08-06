0238041d26af3aa7c67f06a673cbcc4640e85323
Ronald <ronaldpachas89@gmail.com>
2019-08-06 12:01:54 -0500

    Pokedata menú lateral

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-#Pokedex Pokemon Go
-La aplicación permitira listar los distintos pokemones, simulando la pokedex del anime pokemon.
+# PokeMania
+
+## Índice
+
+- [Índice](#índice)
+- [Definición](#Definición)
+- [Resumen](#Resumen)
+- [Historias de Usuario](#Historias-de-Usuario)
+- [Prototipo de baja fidelidad](#Prototipo-de-baja-fidelidad)
+- [Prototipo de Alta fidelidad en Figma](#Prototipo-de-Alta-fidelidad-en-Figma)
+ 
+# Definición
+**Pokemon Go** es un videojuego de realidad aumentada basada en la localizacion. El juego consiste en buscar y capturar personajes de la saga Pokémon escondidos en ubicaciones del mundo real y luchar con ellos, lo que implica desplazarse por las calles de la ciudad, para atrapar a sus pokemones.
+
+Este juego es muy popular entre niños y jóvenes y adultos ,debido a que es un juego simple, fácil de jugar . Pueden acceder a la aplicación todas la personas que deseen jugar. 
+
+# Resumen
+
+**¿QUE ES POKEMANIA?**
+
+**Pokemania** es un sitio web en la cual puedes acceder a la información básica sobre los pokemones de la región Kanto, ayudando al Usuario en su búsqueda por uno o varios pokemones en concreto de manera mas efectiva y rápida.
+
+Para poder ingresar se necesita su nombre de usuario **LABORATORIA** y su contraseña **LABORATORIA** si los datos ingresados son incorrectos te saldra un mensaje.
+
+# Historias de Usuario
+
+*¿Quien es nuestro Usuario?*
+Es un jugador de pokemon go con conocimientos basicos de Pokemones, juega en sus ratos libres. Nuestro Usuario desea que la pagina tenga la siguiente funcionalidad:
+
+*Historia N°1:*
+
+-**COMO:** Jugador de Pokémon Go.
+
+-**QUIERO:** Poder loguearme para acceder al sitio web
+
+-**PARA:** Que tenga mayor seguridad al ingresar
+
+*Historia N°2:*
+
+-**COMO** : Jugador
+
+-**QUIERO**: Poder ver todos los pokemones al ingresar a la pagina
+
+-**PARA** : Visualizar cuantos pokemones tengo y cuantos me falta por atrapar
+
+*Historia N°3:* 
+
+-**COMO** : Jugador
+
+-**QUIERO**: Quiero poder elegir un pokemon 
+
+-**PARA**: Poder vizualizar sus caracteristicas mas relevantes(nombre,tipo,debilidades,etc)
+
+*Historia N°4:*
+
+-**COMO** :Jugador
+
+-**QUIERO**: Poder ordenar por orden alfabetico **(A-Z y Z-A)**  y frecuencia de aparicion de  manera ascendente y descendente.
+
+-**PARA**: Poder ubicarlos fácilmente y saber cuáles son los más difíciles de atrapar.
+
+*Historia N°5:* 
+
+-**COMO**: Jugador
+
+-**QUIERO**: Filtrar a los pokemones por tipo y debilidades
+
+-**PARA**: Saber quienes y cuantos son de cada tipo y quienes tienen esa debilidades.
+
+*Historia N°6:* 
+
+-**COMO**: Jugador
+
+-**QUIERO**: Obtener el porcentaje de los pokemones que aparecen en los diferentes tipo de huevo
+
+-**PARA**: Saber que huevos eclocionar.
+
+# Prototipo de baja fidelidad
+
+ ## *Prototipo en movil*
+
+
+ ## *Prototipo en Web*
+
+
+## *FEEDBACK:*
+
+
+
+# Prototipo de Alta fidelidad en Figma
+//aqui ira mis imagenes
+
+## FEEDBACk
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,196 @@
+//GLOBALES
+const pokeData = POKEMON.pokemon;
+
 /* Manejo del DOM */
 
-document.querySelector("#welcome").classList.add('cargar');
+const menu = document.getElementById('menu');
+const contenedor = document.getElementsByClassName('container');  
+const principal = document.getElementById('principal'); 
 
-const btntoggle = document.querySelector(".mostrar");
+
+//  Mostrar menú
+const btntoggle = document.querySelector(".icono");
 btntoggle.addEventListener('click', ()=>{
-    document.querySelector(".barra2").classList.toggle('active');
-    document.querySelector(".opciones").classList.toggle('hide');
+    document.querySelector(".menu").classList.toggle('abrir');
+    document.querySelector(".icono").classList.toggle('icono-posicion');
+    document.querySelector("#footer").classList.toggle('ancho');
+    const contenedor = document.getElementsByClassName('container');
+    contenedor[0].classList.toggle('ancho');
+    contenedor[1].classList.toggle('ancho');
 });
 
+// animaciones lista del menú
+const drowdown = document.getElementsByClassName('dropdown');
+const listado = document.getElementsByClassName('listado');
+
+for(let i = 0; i < drowdown.length; i++){
+  drowdown[i].addEventListener('click', ()=>{
+    for(let j = 0; j < listado.length; j++){
+      if(i != j){
+        listado[j].classList.remove('activar');
+      }
+    }
+    listado[i].classList.toggle('activar');
+  }); 
+
+}
+
+
+
+// Mostrar Pokemones
+const clases = (objeto)=>{
+    let clase = '';
+    switch (objeto){
+      case "Grass":
+        clase = 'Planta';
+        break;
+      case "Poison":
+        clase = 'Veneno';
+        break;
+      case "Fire":
+        clase = 'fuego';
+        break;
+      case "Flying":
+        clase = 'volador';
+        break;
+      case "Water":
+        clase = 'agua';
+        break;
+      case "Bug":
+        clase = 'bicho';
+        break;
+      case "Normal":
+        clase = 'normal';
+        break;
+      case "Electric":
+        clase = 'electrico';
+        break;
+      case "Ground":
+        clase = 'tierra';
+        break;
+      case "Fighting":
+        clase = 'lucha';
+        break;
+      case "Psychic":
+        clase = 'psiquico';
+        break;
+      case "Rock":
+        clase = 'roca';
+        break;
+      case "Ice":
+        clase = 'hielo';
+        break;
+      case "Ghost":
+        clase = 'fantasma';
+        break;
+      case "Dragon":
+        clase = 'dragon';
+        break;
+    }
+    return clase;
+}
+
+const mostrarTipos = (objeto,num) =>{
+    let tipo = ' ';
+    if (objeto[num].type.length == 2){
+        tipo = ` 
+        <p class="${clases(objeto[num].type[0])}">${objeto[num].type[0]}</p>
+        <p class="${clases(objeto[num].type[1])}">${objeto[num].type[1]}</p>`;
+    }else{
+        tipo = ` 
+        <p class="${clases(objeto[num].type[0])}">${objeto[num].type[0]}</p>`;
+    }
+    return tipo;
+}
+
+const loadPokemon = (objeto) => { 
+	let mostrar = ' ';
+	for (let i = 0; i < objeto.length; i++) {
+        let tipos = mostrarTipos(pokeData,i);
+        let llamar = `
+        <div class="card">
+        <img src="${objeto[i].img}" alt="" class="card-img">
+        <div class="card-body">
+
+          <p class="card-num">N.°${objeto[i].num}</p>
+          <h5 class="card-title">${objeto[i].name}</h5>
+          <p class="card-text">Tipo:</p>
+          <div class="tipo">
+            ${tipos}
+          </div>
+
+        </div>
+      </div> `;
+		mostrar += llamar;
+	}
+	return mostrar;
+};
+
+
+
+principal.innerHTML = loadPokemon(pokeData);
+/*
+    Descripción: Creamos una función que recorra el objeto y luego le pasamos el objeto pokedata para que aplique lo establecido.
+    función mostrarPoke
+    Le pasamos el objeto mostrarPoke(pokeData)
+    Pokedata es nuestro objeto
+*/
+
+const tipos = {
+    tipo : ['Grass','Poison','Fire','Flying','Water','Bug','Normal','Electric','Ground','Fighting','Psychic','Rock','Ice','Ghost','Ghost'],
+    relleno: ['#9bcc50','#b97fc9','#fd7d24']
+};
+
+// Gráfico
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Grass', 'Poison', 'Fire', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+
+    options: {
+      layout: {
+          padding: {
+              left: 30,
+              right: 10,
+              top: 0,
+              bottom:0
+          }
+      },
+      scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+      },
+      title: {
+        display: true,
+        text: 'PokeData',
+        fontSize: 30,
+    },
+    
+  }
+});
